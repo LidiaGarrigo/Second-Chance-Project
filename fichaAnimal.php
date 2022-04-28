d901b16b709d2d150d53de69c1fcef5aceeff18a
LidiaSTD <lidia@std-mr.com>
2022-04-28 11:32:53 +0200

    mejorasLightBox

--- fichaAnimal.php
+++ fichaAnimal.php
@@ -15,6 +15,7 @@ $datos = json_decode($json, true);
     <link rel="stylesheet" href="Css/fichaAnimal.css">
     <link rel="stylesheet" href="Css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;display=swap" rel="stylesheet">
+    <script src="JS/script.js"></script>
 </head>
 
 <body>
@@ -28,10 +29,10 @@ $datos = json_decode($json, true);
                 <div class="column">
                     <img src="assets/animales/perros/perro1/perro1.png" style="width:501px;cursor:pointer;" onclick="openModal();currentSlide(1)">
                     <div style="display:flex; justify-content: center; gap: 7px;">
-                        <img src="assets/animales/perros/perro1/perro1-1.png" style="width:120px;cursor:pointer;" onclick="openModal();currentSlide(1)">
-                        <img src="assets/animales/perros/perro1/perro1-2.png" style="width:120px;cursor:pointer;" onclick="openModal();currentSlide(1)">
-                        <img src="assets/animales/perros/perro1/perro1-3.png" style="width:120px;cursor:pointer;" onclick="openModal();currentSlide(1)">
-                        <img src="assets/animales/perros/perro1/perro1-4.png" style="width:120px;cursor:pointer;" onclick="openModal();currentSlide(1)">
+                        <img src="assets/animales/perros/perro1/perro1-1.png" style="width:120px;cursor:pointer;" onclick="openModal();currentSlide(2)">
+                        <img src="assets/animales/perros/perro1/perro1-2.png" style="width:120px;cursor:pointer;" onclick="openModal();currentSlide(3)">
+                        <img src="assets/animales/perros/perro1/perro1-3.png" style="width:120px;cursor:pointer;" onclick="openModal();currentSlide(4)">
+                        <img src="assets/animales/perros/perro1/perro1-4.png" style="width:120px;cursor:pointer;" onclick="openModal();currentSlide(5)">
                     </div>
                 </div>
 
@@ -43,27 +44,27 @@ $datos = json_decode($json, true);
 
                     <div class="mySlides">
                         <div class="numbertext">1 / 5</div>
-                        <img src="assets/animales/perros/perro1/perro1.png" style="width:100%;">
+                        <img src="assets/animales/perros/perro1/perro1.png" style="width:90%;">
                     </div>
 
                     <div class="mySlides">
                         <div class="numbertext">2 / 5</div>
-                        <img src="assets/animales/perros/perro1/perro1-1.png" style="width:100%">
+                        <img src="assets/animales/perros/perro1/perro1-1.jpg" style="width:90%">
                     </div>
 
                     <div class="mySlides">
                         <div class="numbertext">3 / 5</div>
-                        <img src="assets/animales/perros/perro1/perro1-2.png" style="width:100%">
+                        <img src="assets/animales/perros/perro1/perro1-2.jpg" style="width:90%">
                     </div>
 
                     <div class="mySlides">
                         <div class="numbertext">4 / 5</div>
-                        <img src="assets/animales/perros/perro1/perro1-3.png" style="width:100%">
+                        <img src="assets/animales/perros/perro1/perro1-3.jpg" style="width:90%">
                     </div>
 
                     <div class="mySlides">
                         <div class="numbertext">5 / 5</div>
-                        <img src="assets/animales/perros/perro1/perro1-4.png" style="width:100%">
+                        <img src="assets/animales/perros/perro1/perro1-4.jpg" style="width:90%">
                     </div>
 
                     <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
@@ -94,48 +95,6 @@ $datos = json_decode($json, true);
         </div>
     </div>
 
-    <script>
-        function openModal() {
-            document.getElementById("myModal").style.display = "block";
-        }
-
-        function closeModal() {
-            document.getElementById("myModal").style.display = "none";
-        }
-
-        var slideIndex = 1;
-        showSlides(slideIndex);
-
-        function plusSlides(n) {
-            showSlides(slideIndex += n);
-        }
-
-        function currentSlide(n) {
-            showSlides(slideIndex = n);
-        }
-
-        function showSlides(n) {
-            var i;
-            var slides = document.getElementsByClassName("mySlides");
-            var dots = document.getElementsByClassName("demo");
-            var captionText = document.getElementById("caption");
-            if (n > slides.length) {
-                slideIndex = 1
-            }
-            if (n < 1) {
-                slideIndex = slides.length
-            }
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex - 1].style.display = "block";
-            dots[slideIndex - 1].className += " active";
-            captionText.innerHTML = dots[slideIndex - 1].alt;
-        }
-    </script>
 </body>
 
 </html>
